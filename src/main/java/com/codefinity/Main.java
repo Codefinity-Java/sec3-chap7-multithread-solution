5a76e5eab6927efa2bad5507fad274ccf3293710
Yevhenii Obiedkov <yevhenii.obiedkov@LT-M-6919.local>
2024-08-01 17:02:01 +0300

    version 2.0

--- src/main/java/com/codefinity/Main.java
+++ src/main/java/com/codefinity/Main.java
@@ -1,11 +1,13 @@
 package com.codefinity;
 
 import java.io.File;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         File[] files = {
                 new File("file1.txt"),
                 new File("file2.txt"),
@@ -15,7 +17,8 @@ public class Main {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
 
         for (File file : files) {
-            executorService.submit(new WordCountTask(file));
+            Future<String> submit = executorService.submit(new WordCountTask(file));
+            System.out.println(submit.get());
         }
 
         executorService.shutdown();
--- src/main/java/com/codefinity/WordCountTask.java
+++ src/main/java/com/codefinity/WordCountTask.java
@@ -4,8 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.concurrent.Callable;
 
-public class WordCountTask implements Runnable {
+public class WordCountTask implements Callable<String> {
     private final File file;
 
     public WordCountTask(File file) {
@@ -13,15 +14,18 @@ public class WordCountTask implements Runnable {
     }
 
     @Override
-    public void run() {
+    public String call() {
+        StringBuffer stringBuffer = new StringBuffer();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 int wordCount = line.split("\\s+").length;
-                System.out.println("File: " + file.getName() + ", Line: " + line + ", Word Count: " + wordCount);
+                String result = String.format("File: %s | Line: %s | Word Count %d", file.getName(), line, wordCount);
+                stringBuffer.append(result).append("\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return stringBuffer.toString();
     }
 }
